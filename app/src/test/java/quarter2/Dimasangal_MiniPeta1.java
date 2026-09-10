04592bf90891d8d5f90e2865fa755848463ea035
Denise <dimasangaldenise7@gmail.com>
2026-09-10 15:48:39 +0800

    package quarter2;
    public class Dimasangal_MiniPeta1 {
    }
        public static void main(String[] args) {
            // 1. THE INPUT (Storing your personal details in variables)
            String myName = "Denise";
            String petName = "Lucky";
            String favFood = "Takoyaki";
            int myAge = 16;
    
            // 2. THE OUTPUT (Printing to the console)
            System.out.println("--- MY DIGITAL PROFILE ---");
            System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
            System.out.println("I have a wonderful pet named " + petName + ".");
            System.out.println("If I could, I would eat " + favFood + " every single day!");
        }
    }

---
+++ app/src/test/java/quarter2/Dimasangal_MiniPeta1.java
@@ -0,0 +1,18 @@
+package quarter2;
+
+public class Dimasangal_MiniPeta1 {
+}
+public static void main(String[] args) {
+    // 1. THE INPUT (Storing your personal details in variables)
+    String myName = "Denise";
+    String petName = "Lucky";
+    String favFood = "Takoyaki";
+    int myAge = 16;
+
+    // 2. THE OUTPUT (Printing to the console)
+    System.out.println("--- MY DIGITAL PROFILE ---");
+    System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+    System.out.println("I have a wonderful pet named " + petName + ".");
+    System.out.println("If I could, I would eat " + favFood + " every single day!");
+}
+}
