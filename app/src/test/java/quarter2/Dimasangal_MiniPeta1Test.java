54e38f10b6cc69740f1adc0f6ed8dcc0fc2604b0
Denise <dimasangaldenise7@gmail.com>
2026-09-12 00:11:31 +0800

    Added Dimasangal_MiniPeta1Test.java

--- app/src/test/java/quarter2/Dimasangal_MiniPeta1Test.java
+++ app/src/test/java/quarter2/Dimasangal_MiniPeta1Test.java
@@ -1,32 +1,22 @@
 package quarter2;
 
-import org.junit.Test
+import org.junit.Test;
 
-class Dimasangal_MiniPeta1Test {
+public class Dimasangal_MiniPeta1Test {
+
     @Test
-    fun printMyProfile() {
-        // Personal details
+    public void testProfile() {
 
-        val myName = "Denise"
-        val petName = "Lucky"
-        val favFood = "Takoyaki"
-        val myAge = 15
+        // 1. THE INPUT (Storing your personal details in variables)
+        String myName = "Denise";
+        String petName = "Lucky";
+        String favFood = "Takoyaki";
+        int myAge = 15;
 
-        // Display profile
-        println("===== MY DIGITAL PROFILE =====")
-
-        println(
-                "Hello, my name is " + myName +
-                        " and I am " + myAge + " years old."
-        )
-
-        println(
-                "I have a wonderful pet named " + petName + "."
-        )
-
-        println(
-                "If I could, I would eat " + favFood +
-                        " every single day!"
-        )
+        // 2. THE OUTPUT (Printing to the console)
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
     }
 }
