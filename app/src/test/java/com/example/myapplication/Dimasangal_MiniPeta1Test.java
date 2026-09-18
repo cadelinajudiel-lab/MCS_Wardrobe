e85a6b7a513bd397c076ae4ab0b0a4506e86e18a
Denise <dimasangaldenise7@gmail.com>
2026-09-18 13:57:06 +0800

    Added Dimasangal_MiniPeta1Test

--- app/src/test/java/com/example/myapplication/Dimasangal_MiniPeta1Test.java
+++ app/src/test/java/com/example/myapplication/Dimasangal_MiniPeta1Test.java
@@ -9,7 +9,7 @@ public class Dimasangal_MiniPeta1Test {
         String myName = "Denise";
         String petName = "Lucky";
         String favFood = "Takoyaki";
-        int myAge = 15;
+        int myAge = 16;
 
         // 2. THE OUTPUT (Printing to the console)
         System.out.println("--- MY DIGITAL PROFILE ---");
