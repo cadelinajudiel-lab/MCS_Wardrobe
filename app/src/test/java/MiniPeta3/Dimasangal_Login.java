2ef32f65cf67d4dbc6d907cd78f846c5289a2fd9
Denise <dimasangaldenise7@gmail.com>
2026-09-20 22:05:08 +0800

    Added Dimasangal_LoginMiniPeta3Test.java

---
+++ app/src/test/java/MiniPeta3/Dimasangal_Login.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import org.junit.Test;
+
+public class Dimasangal_Login {
+
+    @Test
+
+    public void main() {
+        boolean isReadyToLogin = true;
+
+        // Stores username
+        String username = "DeniseDimasangal";
+
+        // Stores password
+        String password = "DD92710";
+
+        if (isReadyToLogin) {
+            System.out.println("Login Successful");
+            System.out.println("Username: " + username);
+            System.out.println("Password: " + password);
+        } else {
+            System.out.println("Login Failed");
+        }
+    }
+}
