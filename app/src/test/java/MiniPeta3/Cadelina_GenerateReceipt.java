b5d775bf6d16940134b9b4588ec02ec56445ce26
Cadelina <cadelinajudiel@gmail.com>
2026-09-22 15:09:30 +0800

    Added Dimasangal_Login.java

--- app/src/test/java/MiniPeta3/Cadelina_GenerateReceipt.java
+++ app/src/test/java/MiniPeta3/Cadelina_GenerateReceipt.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package MiniPeta3;
 
 import org.junit.Test;
 import java.util.Scanner;
--- app/src/test/java/MiniPeta3/Cadeliña_PlaceOrder.java
+++
@@ -1,9 +0,0 @@
-public class Cadeliña_PlaceOrder{
-
-   public static void main(String[] args){
-
-
-
-}
-
-}
