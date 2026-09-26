3d6277706a4b5236960afc35a283d9e4963c55a7
Denise <dimasangaldenise7@gmail.com>
2026-09-26 23:15:37 +0800

    Added FIXING ERRORS AND UNNECESSARY FILES

--- app/src/test/java/Practical/Exam/Cadelina_ArcadeCounterTest.java
+++ app/src/test/java/Practical/Exam/Cadelina_ArcadeCounterTest.java
@@ -1,15 +1,15 @@
-  package  com.example.myapplication.practical.exam
+package  com.example.myapplication.practical.exam
 
-  import org.junit.Test;
-  import java.io.ByteArrayInputStream;
-  import java.util.Scanner;
-  public class Cadelina_ArcadeCounterTest {
+import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+public class Cadelina_ArcadeCounterTest {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
 
 
-        }
+    }
 
 
 }
